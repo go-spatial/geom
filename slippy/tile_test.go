83b97c7023d74741590f63cfa23ffcca587ca401
Gautam Dey <gautam.dey77@gmail.com>
2018-05-02 10:37:52 -0700

    Initial common planer functions
    added NewTileLatLon funcitonnality
    added an iterator function RangeFamilyAt to Tile
    Added tests and bug fixes for lat/lon -> tile conversion
    Added reference to the formula for PerpendicularDisance.
    Added references to docs used for algos.
    
    Removing spherical functions as they are not correct.

--- slippy/tile_test.go
+++ slippy/tile_test.go
@@ -111,3 +111,173 @@ func TestNewTile(t *testing.T) {
 	}
 
 }
+
+func TestNewTileLatLon(t *testing.T) {
+	type tcase struct {
+		z, x, y  uint
+		lat, lon float64
+		buffer   float64
+		srid     uint64
+	}
+	fn := func(t *testing.T, tc tcase) {
+
+		// Test the new functions.
+		tile := slippy.NewTileLatLon(tc.z, tc.lat, tc.lon, tc.buffer, tc.srid)
+		{
+			gz, gx, gy := tile.ZXY()
+			if gz != tc.z {
+				t.Errorf("z, expected %v got %v", tc.z, gz)
+			}
+			if gx != tc.x {
+				t.Errorf("x, expected %v got %v", tc.x, gx)
+			}
+			if gy != tc.y {
+				t.Errorf("y, expected %v got %v", tc.y, gy)
+			}
+			if tile.Buffer != tc.buffer {
+				t.Errorf("buffer, expected %v got %v", tc.buffer, tile.Buffer)
+			}
+			if tile.SRID != tc.srid {
+				t.Errorf("srid, expected %v got %v", tc.srid, tile.SRID)
+			}
+		}
+
+	}
+	tests := map[string]tcase{
+		"zero":{
+			z:      0,
+			x:      0,
+			y:      0,
+			lat: 0,
+			lon: 0,
+			buffer: 64,
+			srid:   geom.WebMercator,
+		},
+		"center": {
+			z:      8,
+			x:      128,
+			y:      128,
+			lat: 0,
+			lon: 0,
+			buffer: 64,
+			srid:   geom.WebMercator,
+		},
+		"arbitrary zoom 2": {
+			z:      2,
+			x:      2,
+			y:      3,
+			lat: -70,
+			lon: 20,
+			buffer: 64,
+			srid:   geom.WebMercator,
+		},
+		"arbitrary zoom 16": {
+			z:      16,
+			x:      11436,
+			y:      26461,
+			lat: 32.705,
+			lon: -117.176,
+			buffer: 64,
+			srid:   geom.WebMercator,
+		},
+	}
+
+	for k, tc := range tests {
+		tc := tc
+		t.Run(k, func(t *testing.T) { fn(t, tc) })
+	}
+}
+
+func TestRangeFamilyAt(t *testing.T) {
+	type coord struct {
+		z, x, y uint
+	}
+
+	testcases := map[string]struct {
+		tile     *slippy.Tile
+		zoomAt   uint
+		expected []coord
+	}{
+		"children 1": {
+			tile:   slippy.NewTile(0, 0, 0, 0, geom.WebMercator),
+			zoomAt: 1,
+			expected: []coord{
+				{1, 0, 0},
+				{1, 0, 1},
+				{1, 1, 0},
+				{1, 1, 1},
+			},
+		},
+		"children 2": {
+			tile:   slippy.NewTile(8, 3, 5, 0, geom.WebMercator),
+			zoomAt: 10,
+			expected: []coord{
+				{10, 12, 20},
+				{10, 12, 21},
+				{10, 12, 22},
+				{10, 12, 23},
+				//
+				{10, 13, 20},
+				{10, 13, 21},
+				{10, 13, 22},
+				{10, 13, 23},
+				//
+				{10, 14, 20},
+				{10, 14, 21},
+				{10, 14, 22},
+				{10, 14, 23},
+				//
+				{10, 15, 20},
+				{10, 15, 21},
+				{10, 15, 22},
+				{10, 15, 23},
+			},
+		},
+		"parent 1": {
+			tile: slippy.NewTile(1, 0, 0, 0, geom.WebMercator),
+			zoomAt: 0,
+			expected: []coord{
+				{0, 0, 0},
+			},
+		},
+		"parent 2": {
+			tile: slippy.NewTile(3, 3, 5, 0, geom.WebMercator),
+			zoomAt: 1,
+			expected: []coord{
+				{1, 0, 1},
+			},
+		},
+	}
+
+	isIn := func(arr []coord, c coord) bool {
+		for _, v := range arr {
+			if v == c {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	for k, tc := range testcases {
+		coordList := make([]coord, 0, len(tc.expected))
+		tc.tile.RangeFamilyAt(tc.zoomAt, func(tile *slippy.Tile) error {
+			z, x, y := tile.ZXY()
+			c := coord{z, x, y}
+
+			coordList = append(coordList, c)
+
+			return nil
+		})
+
+		if len(coordList) != len(tc.expected) {
+			t.Fatalf("[%v] expected coordinate list of length %d, got %d", k, len(tc.expected), len(coordList))
+		}
+
+		for _, v := range tc.expected {
+			if !isIn(coordList, v) {
+				t.Fatalf("[%v] expected coordinate %v missing from list %v", k, v, coordList)
+			}
+		}
+	}
+}
