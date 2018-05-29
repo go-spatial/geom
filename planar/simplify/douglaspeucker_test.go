0b58be86c8b07b9860e050dfc8b6522fab45a772
Gautam Dey <gautam.dey77@gmail.com>
2018-05-29 13:57:48 -0700

    Douglaspeucker (#7)
    * Basic Planar fns, Simplifier interface
    
    Added some handy planar functions, and defined an Interface for
    Simplifers. This way we can add and change out simplifiers easily.
    
    * Added doublaspeucker simplification algo.
    
    * Added reference to the formula for PerpendicularDisance.
    
    * Updated according to reviews.
    
    Moved spherical functions to the spherical namespace.
    Added references to docs used for algos.
    
    * Removed haversine test for simplify. Because not sure if it is working.
    
    * [issue #7] Pull out Douglaspeucker Algo to the geom package.
    
    Removed the spherical version as we don't need it and there may be an
    issue with the distance formula that was being used, and got pulled.
    
    Improved test scaffolding, with the aim to add more tests.

---
+++ planar/simplify/douglaspeucker_test.go
@@ -0,0 +1,63 @@
+package simplify
+
+import (
+	"flag"
+	"reflect"
+	"testing"
+)
+
+var ignoreSanityCheck bool
+
+func init() {
+	flag.BoolVar(&ignoreSanityCheck, "ignoreSanityCheck", false, "ignore sanity checks in test cases.")
+}
+
+func TestDouglasPeucker(t *testing.T) {
+	type tcase struct {
+		l  [][2]float64
+		dp DouglasPeucker
+		el [][2]float64
+	}
+
+	fn := func(t *testing.T, tc tcase) {
+		gl, err := tc.dp.Simplify(tc.l, false)
+		// Douglas Peucker should never return an error.
+		// This is more of a sanity check.
+		if err != nil {
+			t.Errorf("Douglas Peucker error, expected nil got %v", err)
+			return
+		}
+		if !reflect.DeepEqual(tc.el, gl) {
+			t.Errorf("simplified points, expected %v got %v", tc.el, gl)
+			return
+		}
+
+		if ignoreSanityCheck {
+			return
+		}
+
+		// Let's try it with true, it should not matter, as DP does not care.
+		// More sanity checking.
+		gl, _ = tc.dp.Simplify(tc.l, true)
+
+		if !reflect.DeepEqual(tc.el, gl) {
+			t.Errorf("simplified points (true), expected %v got %v", tc.el, gl)
+			return
+		}
+	}
+
+	tests := map[string]tcase{
+		"simple box": {
+			l: [][2]float64{{0, 0}, {0, 1}, {1, 1}, {1, 0}},
+			dp: DouglasPeucker{
+				Tolerance: 0.001,
+			},
+			el: [][2]float64{{0, 0}, {0, 1}, {1, 1}, {1, 0}},
+		},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) { fn(t, tc) })
+	}
+}
