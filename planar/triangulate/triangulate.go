fcb58cab1e6aaf0ebcd3086798f986b9bd28f326
Gautam Dey <gautam.dey77@gmail.com>
2018-08-24 11:07:04 -0700

    Uodated Triangulator and Constrainer interfaces.
    Updated interfaces according to feedback.

--- planar/triangulate/triangulate.go
+++ planar/triangulate/triangulate.go
@@ -2,15 +2,31 @@ package triangulate
 
 import "github.com/go-spatial/geom"
 
-type Interface interface {
+// Triangulator describes an object that can take a set of points and produce
+// a triangulation.
+type Triangulator interface {
+
 	// SetPoints sets the nodes to be used in the triangulation
-	SetPoints(pts []geom.Point, data []interface{})
+	// data is any metadata to be attached to each point.
+	// 	This is a one to one mapping. The length of data MUST be either zero,
+	// 	or equal to the length of points
+	SetPoints(points []geom.Point, data []interface{}) error
+
 	// Triangles returns the triangles that are produced by the triangulation
-	Triangles() []geom.Triangle
+	// 	If the triangulation uses a frame the includeFrame should be used to
+	// 	determine if the triangles touching the frame should be included or not.
+	Triangles(includeFrame bool) ([]geom.Triangle, error)
 }
 
+// Constrainer is a Triangulator that can take a set of points and ensure that the
+// given set of edges (the constraints) exist in the triangulation
 type Constrainer interface {
+	Triangulator
+
 	// AddConstraints adds constraint lines to the triangulation, this may require
 	// the triangulation to be recalculated.
-	AddConstraints(constraints ...geom.Line)
+	// data is any metadata to be attached to each constraint.
+	// 	This is a one to one mapping. The length of data MUST be either zero,
+	// 	or equal to the length of constraints
+	AddConstraints(constraints []geom.Line, data []interface{}) error
 }
