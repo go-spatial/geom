4d7e2d3772bc73a30bc2f11596e476ce35efcd7a
Gautam Dey <gautam.dey77@gmail.com>
2019-03-19 16:22:40 -0700

    Updated with comments in discussion.

--- planar/triangulate/triangulate.go
+++ planar/triangulate/triangulate.go
@@ -1,32 +1,45 @@
 package triangulate
 
-import "github.com/go-spatial/geom"
+import (
+	"context"
+
+	"github.com/go-spatial/geom"
+)
 
 // Triangulator describes an object that can take a set of points and produce
 // a triangulation.
 type Triangulator interface {
 
-	// SetPoints sets the nodes to be used in the triangulation
-	// data is any metadata to be attached to each point.
-	// 	This is a one to one mapping. The length of data MUST be either zero,
-	// 	or equal to the length of points
-	SetPoints(points []geom.Point, data []interface{}) error
+	// SetPoints sets the nodes to be used in the triangulation, this will replace
+	// any current points in the triangulation
+	SetPoints(ctx context.Context, points ...geom.Point) error
 
 	// Triangles returns the triangles that are produced by the triangulation
-	// 	If the triangulation uses a frame the includeFrame should be used to
-	// 	determine if the triangles touching the frame should be included or not.
-	Triangles(includeFrame bool) ([]geom.Triangle, error)
+	Triangles(ctx context.Context, includeFrame bool) ([]geom.Triangle, error)
 }
 
-// Constrainer is a Triangulator that can take a set of points and ensure that the
+// IncrementatlTriangulator describes an triangulation where points can be
+// incrementally added.
+type IncrementalTriangulator interface {
+	Triangulator
+
+	// InsertPoint inserts a point into an existing triangulation
+	InsertPoints(ctx context.Context, points ...geom.Point) error
+}
+
+// ConstrainedTriangulator is a Triangulator that can take a set of points and ensure that the
 // given set of edges (the constraints) exist in the triangulation
-type Constrainer interface {
+type ConstrainedTriangulator interface {
 	Triangulator
 
 	// AddConstraints adds constraint lines to the triangulation, this may require
 	// the triangulation to be recalculated.
-	// data is any metadata to be attached to each constraint.
-	// 	This is a one to one mapping. The length of data MUST be either zero,
-	// 	or equal to the length of constraints
-	AddConstraints(constraints []geom.Line, data []interface{}) error
+	AddConstraints(ctx context.Context, constraints ...geom.Line) error
+}
+
+// ConstrainedIncrementalTriangulator is an Incremental Triangulator that can take a set of points and ensure that the
+// given set of edges (the constraints) exist in the triangulation
+type ConstrainedIncrementalTriangulator interface {
+	IncrementalTriangulator
+	ConstrainedTriangulator
 }
