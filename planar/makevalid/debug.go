423af7982607a55a19b5511ca0f6fbbffbc4db78
henrythasler <git@thasler.org>
2019-04-11 19:43:58 +0200

    turn off debugging

--- planar/makevalid/debug.go
+++ planar/makevalid/debug.go
@@ -2,7 +2,7 @@ package makevalid
 
 import "github.com/go-spatial/geom/internal/debugger"
 
-const debug = true
+const debug = false
 
 const (
 	debuggerCategoryTriangle = debugger.CategoryJoiner("triangle:")
