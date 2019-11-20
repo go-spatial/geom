94c3983e0e909c4ccaef1701f9c1d87e617c1a00
Gautam Dey <gautam.dey77@gmail.com>
2019-11-20 12:29:48 -0800

    [quadedge] Removed hard coded inifinite loop panic

--- planar/triangulate/delaunay/quadedge/quadedge.go
+++ planar/triangulate/delaunay/quadedge/quadedge.go
@@ -15,6 +15,8 @@
 //
 package quadedge
 
+import "sync/atomic"
+
 // QuadEdge describes a quadedge object. Which is made up of four directional edges
 type QuadEdge struct {
 	initialized bool
@@ -23,7 +25,10 @@ type QuadEdge struct {
 
 // NewQEdge create a new quad edge object
 func NewQEdge() *QuadEdge {
+	var gidx = atomic.AddUint64(&glbIdx, 1)
 	var qe QuadEdge
+
+	qe.e[0].glbIdx, qe.e[1].glbIdx, qe.e[2].glbIdx, qe.e[3].glbIdx = gidx, gidx, gidx, gidx
 	qe.e[0].num, qe.e[1].num, qe.e[2].num, qe.e[3].num = 0, 1, 2, 3
 	qe.e[0].qe, qe.e[1].qe, qe.e[2].qe, qe.e[3].qe = &qe, &qe, &qe, &qe
 
