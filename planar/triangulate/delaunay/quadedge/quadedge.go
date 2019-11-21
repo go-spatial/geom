63349ccd0437a509ad49272f826473a22011aea0
Gautam Dey <gautam.dey77@gmail.com>
2019-11-20 20:33:04 -0800

    [quadedge] refactor

--- planar/triangulate/delaunay/quadedge/quadedge.go
+++ planar/triangulate/delaunay/quadedge/quadedge.go
@@ -23,14 +23,12 @@ type QuadEdge struct {
 	e           [4]Edge
 }
 
-// NewQEdge create a new quad edge object
-func NewQEdge() *QuadEdge {
+func (qe *QuadEdge) Init() {
 	var gidx = atomic.AddUint64(&glbIdx, 1)
-	var qe QuadEdge
 
 	qe.e[0].glbIdx, qe.e[1].glbIdx, qe.e[2].glbIdx, qe.e[3].glbIdx = gidx, gidx, gidx, gidx
 	qe.e[0].num, qe.e[1].num, qe.e[2].num, qe.e[3].num = 0, 1, 2, 3
-	qe.e[0].qe, qe.e[1].qe, qe.e[2].qe, qe.e[3].qe = &qe, &qe, &qe, &qe
+	qe.e[0].qe, qe.e[1].qe, qe.e[2].qe, qe.e[3].qe = qe, qe, qe, qe
 
 	qe.e[0].next = &(qe.e[0])
 	qe.e[1].next = &(qe.e[3])
@@ -38,6 +36,11 @@ func NewQEdge() *QuadEdge {
 	qe.e[3].next = &(qe.e[1])
 
 	qe.initialized = true
+}
 
+// NewQEdge create a new quad edge object
+func NewQEdge() *QuadEdge {
+	var qe QuadEdge
+	qe.Init()
 	return &qe
 }
